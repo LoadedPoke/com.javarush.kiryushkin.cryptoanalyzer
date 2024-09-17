c8902de73dc0712cc7897b84c45fdc1e8b260462
loadedpoke <loadedpoke@gmail.com>
2024-09-17 13:38:12 +0300

    Rewrite createCharMapping methode

--- src/com/javarush/kiryushkin/cryptoanalyzer/Cipher.java
+++ src/com/javarush/kiryushkin/cryptoanalyzer/Cipher.java
@@ -7,27 +7,21 @@ public class Cipher {
 
     private static final char[] ALPHABET = {'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з',
             'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
-            'ъ', 'ы', 'ь', 'э', 'ю', 'я', '.', ',', '«', '»', '"', '\'', ':', '!', '?', ' '};
+            'ъ', 'ы', 'ь', 'э', 'ю', 'я',
+            '.', ',', '«', '»', '"', '\'', '/', ':', '!', '?', ' ',
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
 
-    private Map<Character, Character> encryptCharMapping;
-    private Map<Character, Character> decryptCharMapping;
+    private Map<Character, Character> сharMapping;
 
     public Cipher(int key) {
-        this.encryptCharMapping = createCharMapping(ALPHABET, key);
-        this.decryptCharMapping = createCharMapping(ALPHABET, -key);
+        this.сharMapping = createCharMapping(ALPHABET, key);
     }
 
     private Map<Character, Character> createCharMapping(char[] charArray, int shift) {
-        Map<Character, Character> charMapping = new HashMap<>();
         if (shift < 0) {
-            shift = -shift;
-            char temp;
-            for (int i = 0; i < charArray.length / 2; i++) {
-                temp = charArray[charArray.length - 1 - i];
-                charArray[charArray.length - 1 - i] = charArray[i];
-                charArray[i] = temp;
-            }
+            shift = charArray.length + shift % charArray.length;
         }
+        Map<Character, Character> charMapping = new HashMap<>();
         for (int i = 0; i < charArray.length; i++) {
             Character key = charArray[i];
             Character value = charArray[(i + shift) % charArray.length];
@@ -37,10 +31,7 @@ public class Cipher {
     }
 
     public char encrypt(char symbol) {
-        return encryptCharMapping.get(symbol);
+        return сharMapping.get(symbol);
     }
 
-    public char decrypt(char symbol) {
-        return decryptCharMapping.get(symbol);
-    }
 }
--- src/com/javarush/kiryushkin/cryptoanalyzer/FileHandler.java
+++ src/com/javarush/kiryushkin/cryptoanalyzer/FileHandler.java
@@ -1,15 +1,17 @@
 package com.javarush.kiryushkin.cryptoanalyzer;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class FileHandler {
 
-    public void readTextFromFile(String filename) throws FileNotFoundException {
-        FileReader fileReader = new FileReader(filename);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
+    public void readTextFromFile(String filename) throws IOException {
+        try (BufferedReader bufferedReader = new BufferedReader(
+                new FileReader(filename))
+        ) {
 
+        }
     }
 
 }
